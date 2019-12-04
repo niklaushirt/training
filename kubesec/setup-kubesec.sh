76835c55a2f787c93858df0ce27f52223daecbd3
Niklaus Hirt <niklaushirt@gmail.com>
2019-12-04 09:25:40 +0100

    kubetest

---
+++ kubesec/setup-kubesec.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+wget https://github.com/controlplaneio/kubesec/releases/download/v2.3.0/kubesec_linux_amd64.tar.gz
+tar xzf kubesec_linux_amd64.tar.gz
+sudo mv kubesec /usr/local/bin
+
+cd ~/training
+
