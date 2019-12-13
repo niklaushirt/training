6a8829b8e74b69561109de84ebc1d908e49dfc3e
Niklaus Hirt <niklaushirt@gmail.com>
2019-12-13 10:39:16 +0100

    117

--- tools/minikube-117.sh
+++ tools/minikube-117.sh
@@ -1,9 +1,3 @@
 #/bin/bash
 
 sudo minikube start --vm-driver=none --memory=8192 --cpus=4 --network-plugin=cni --kubernetes-version v1.17.0
-
-sleep 120
-sudo chown -R $USER ~/.kube ~/.minikube
-minikube dashboard &
-
-
