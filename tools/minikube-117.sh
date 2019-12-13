4fd535bfa6e8a327a9daead31c11cc9620340af4
Niklaus Hirt <niklaushirt@gmail.com>
2019-12-13 11:05:28 +0100

    dashboard

--- tools/minikube-117.sh
+++ tools/minikube-117.sh
@@ -1,3 +1,8 @@
 #/bin/bash
 
+
+sudo mv /home/training/.kube /home/training/.minikube $HOME
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
+
+
 sudo minikube start --vm-driver=none --memory=8192 --cpus=4 --network-plugin=cni --kubernetes-version v1.17.0
--- tools/startup.sh
+++ tools/startup.sh
@@ -1,9 +1,18 @@
 #/bin/bash
 
+
+
 sudo minikube start --vm-driver=none --memory=8192 --cpus=4 --network-plugin=cni --kubernetes-version v1.17.0
 
 sleep 120
+
+sudo mv /home/training/.kube /home/training/.minikube $HOME
+sudo chown -R $USER $HOME/.kube $HOME/.minikube
+
 sudo chown -R $USER ~/.kube ~/.minikube
+
 minikube dashboard &
+
+kubectl patch service -n kubernetes-dashboard kubernetes-dashboard -p '{"spec": {"ports": [{"nodePort": 31719,"port": 80}],"type": "NodePort"}}'
 
 
