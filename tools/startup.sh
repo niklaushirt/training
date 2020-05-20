60c59e9438213644bbf5a3e6dca3e00b4526c827
Niklaus Hirt <niklaushirt@gmail.com>
2020-05-20 11:04:29 +0200

    dashboard

--- tools/startup.sh
+++ tools/startup.sh
@@ -11,7 +11,6 @@ echo "${GREEN}******************************************************************
 echo " ${CYAN}    Starting up your Minikube Training Environment  ${NC}"
 echo " ${CYAN}    ---------------------------------------------  ${NC}"
 echo " ${NC}    Don't worry if there are some errors displayed, this is due to   ${NC}"
-echo " ${NC}    You will have to enter a name that will be used to show your progress in the Instructor Dashboard  ${NC}"
 echo "${GREEN}********************************************************************************${NC}"
 echo "  "
 echo "  "
@@ -20,7 +19,7 @@ echo "  "
 
 echo "${GREEN}--------------------------------------------------------------------------------${NC}"
 echo " ${CYAN}    Starting up Docker ${NC}"
-docker start portainer
+docker start portainer > /dev/null
 
 echo "${GREEN}--------------------------------------------------------------------------------${NC}"
 echo " ${CYAN}    Starting up your Minikube  ${NC}"
@@ -29,12 +28,12 @@ minikube start
 
 echo "${GREEN}--------------------------------------------------------------------------------${NC}"
 echo " ${CYAN}    Creating Registry  ${NC}"
-kubectl apply -f ./training/tools/kube-registry.yaml
+kubectl apply -f ./training/tools/kube-registry.yaml > /dev/null
 kubectl port-forward --namespace kube-system $(kubectl get po -n kube-system | grep kube-registry-v0 | \awk '{print $1;}') 5000:5000  > /dev/null&
 
 echo "${GREEN}--------------------------------------------------------------------------------${NC}"
 echo " ${CYAN}    Creating CNI Cilium  ${NC}"
-kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.6/install/kubernetes/quick-install.yaml
+kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.6/install/kubernetes/quick-install.yaml > /dev/null
 
 echo "${GREEN}--------------------------------------------------------------------------------${NC}"
 echo " ${ORANGE}    Waiting 1 minute to start Kubernetes Dashboard  ${NC}"
