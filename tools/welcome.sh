3ea1b108cc99aaa56fc9a2ee520f8860635d9ba4
Niklaus Hirt <niklaushirt@gmail.com>
2020-10-05 12:20:30 +0200

    welcome

--- tools/welcome.sh
+++ tools/welcome.sh
@@ -1,14 +1,22 @@
 #/bin/bash
 
+echo "${GREEN}--------------------------------------------------------------------------------${NC}"
+echo " ${ORANGE}    Updating Training Files  ${NC}"
+
 cd ./training
 git checkout origin/master -f
 git checkout master -f 
 git pull origin master
 cd
 
+echo "${GREEN}--------------------------------------------------------------------------------${NC}"
+echo " ${ORANGE}    Start Training Content  ${NC}"
 
 firefox https://niklaushirt.github.io/k8s_training_web/
 
+
+echo "${GREEN}--------------------------------------------------------------------------------${NC}"
+echo " ${ORANGE}    Start Training Environment  ${NC}"
 ./training/tools/startEnvironment.sh
 
 # ./training/pte/pte.sh
