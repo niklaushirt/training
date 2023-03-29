292cfc69d8e4cfbbe434eed6ddeea44b7384abcf
Niklaus Hirt <nikh@ch.ibm.com>
2023-03-29 19:33:24 +0200

    Demo

--- demo-app/tomcat/TRAINING_DEMO.sh
+++ demo-app/tomcat/TRAINING_DEMO.sh
@@ -7,17 +7,21 @@ git clone https://github.com/niklaushirt/training.git
 
 cd ~/training/demo-app/tomcat
 podman build -t myimage:1.0.0 .
-
 podman images 
 
-podman run --rm --name myimage -p 8080:8080 myimage:1.0.0
+
+
+podman run --rm -d --name myimage -p 8080:8080 myimage:1.0.0
+podman ps 
+open http://localhost:8080/yourwarfile/
+
+podman kill <CONTAINER_ID>
+
+
 
 podman login docker.io
-
 podman tag myimage:1.0.0 niklaushirt/myimage:1.0.0
-
 podman images 
-
 podman push niklaushirt/myimage:1.0.0
 
 
