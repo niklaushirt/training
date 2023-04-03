ea966b3995a3e2563acba609cb0f239d8f75ac81
Niklaus Hirt <nikh@ch.ibm.com>
2023-04-03 09:57:57 +0200

    training

--- demo-app/tomcat/TRAINING_DEMO.sh
+++ demo-app/tomcat/TRAINING_DEMO.sh
@@ -10,6 +10,12 @@ podman build -t myimage:1.0.0 .
 podman images 
 
 
+podman run --rm -ti myimage:1.0.0 /bin/bash
+
+cat /etc/os-release
+uname -r
+apt-get install git
+
 
 podman run --rm -d --name myimage -p 8080:8080 myimage:1.0.0
 podman ps 
@@ -23,6 +29,8 @@ podman login docker.io
 podman tag myimage:1.0.0 niklaushirt/myimage:1.0.0
 podman images 
 podman push niklaushirt/myimage:1.0.0
+
+open https://hub.docker.com/repositories/niklaushirt
 
 
 
