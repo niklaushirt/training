a7fff9925027346da11348de2e363d35b91709a8
Niklaus Hirt <nikh@ch.ibm.com>
2025-03-17 13:13:25 +0100

    Update

--- COMMANDLINE_COURSE.sh
+++ COMMANDLINE_COURSE.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/niklaushirt/training.git
 cd training
 
+https://hub.docker.com
+
 
 #############################################################################################
 ## BUILD
@@ -33,6 +35,7 @@ podman --connection podman-machine-default-root run \
 https://localhost:9443
 
 podman --connection podman-machine-default-root kill portainer
+podman --connection podman-machine-default-root volume rm portainer_data
 
 #############################################################################################
 ## RUN
@@ -54,19 +57,21 @@ podman stop/kill <CONTAINER_ID>
 #############################################################################################
 ## REGISTRY
 
-podman login quay.io
+podman login docker.io
 
-podman tag myimage:1.0.0 quay.io/niklaushirt/myimage:1.0.0
+podman tag myimage:1.0.0 docker.io/niklaushirt/myimage:1.0.0
 
 podman images
-podman push quay.io/niklaushirt/myimage:1.0.0
+podman push docker.io/niklaushirt/myimage:1.0.0
 
 podman pull docker.io/niklaushirt/myimage:latest
+
+https://hub.docker.com/u/niklaushirt
 
 
 
 podman build --platform linux/arm64/v8 --platform linux/amd64 -t myimage:1.0.0 --manifest myimage-mf .
-podman manifest inspect myimage-mf
+podman manifest inspect myimage-mf|yq -P
 
 
 
@@ -104,6 +109,7 @@ kubectl get pods
 kubectl get pods nginx -o yaml|yq
 
 kubectl get pods
+echo "--------"
 kubectl get pods -l release=april2023
 
 
@@ -117,13 +123,11 @@ kubectl get pods -l release=april2023
 kubectl create -f ./deployment/service.yaml
 
 
-kubectl port-forward --namespace default nginx 8000:80
+kubectl port-forward --namespace default pod/nginx 8000:80
 
 http://localhost:8000
 
 
-kubectl delete -f ./deployment/pod.yaml
-kubectl delete pods nginx-demo
 
 
 #############################################################################################
@@ -137,9 +141,11 @@ kubectl apply -f ./deployment/deployment.yaml
 kubectl get pods
 kubectl get deployments
 
-kubectl port-forward --namespace default $(kubectl get po -n default -l app=nginx | grep nginx | \awk '{print $1;}'|head -1) 8000:80
+kubectl port-forward --namespace default deployment/nginx-deployment 8000:80
 
 http://localhost:8000
+
+kubectl port-forward --namespace default service/nginx-service 8000:8000
 
 
 #############################################################################################
@@ -151,40 +157,29 @@ kubectl apply -f ./deployment/deployment-cm.yaml
 
 
 
-
-
-kubectl delete -f ./deployment/deployment.yaml
-kubectl delete -f ./deployment/deployment-cm.yaml
-
 #############################################################################################
 #############################################################################################
 ## APPLIED
 
 https://niklaushirt.github.io/k8s_training_web/04-kubernetes-basics/04-kubernetes-basics-03
 
-https://hub.docker.com
-
 
 bat ./deployment/demoapp-backend-mac.yaml
+
+bat ./deployment/demoapp-mac.yaml
+
+
 kubectl create -f ./deployment/demoapp-backend-mac.yaml
 kubectl create -f ./deployment/demoapp-backend-service.yaml
 
-
-bat ./deployment/demoapp-mac.yaml
 kubectl create -f ./deployment/demoapp-mac.yaml
 kubectl create -f ./deployment/demoapp-service.yaml
 
 
-
-
-kubectl port-forward --namespace default $(kubectl get po -n default -l app=k8sdemo | grep k8sdemo | \awk '{print $1;}') 3000:3000
+kubectl port-forward --namespace default service/k8sdemo-service 3000:3000
 
 http://localhost:3000
 
-kubectl delete -f ./deployment/demoapp-backend-mac.yaml
-kubectl delete -f ./deployment/demoapp-backend-service.yaml
-kubectl delete -f ./deployment/demoapp-mac.yaml
-kubectl delete -f ./deployment/demoapp-service.yaml
 
 
 
@@ -216,10 +211,6 @@ CREATE DATABASE testing;
 show databases;
 exit;
 
-
-kubectl delete -f ./volumes/3-simple-mysql-deployment.yaml
-kubectl delete -f ./volumes/2-simple-mysql-pvc.yaml
-kubectl delete -f ./volumes/1-simple-mysql-pv.yaml
 
 
 
@@ -256,17 +247,75 @@ bat ./deployment/deployment-LIVENESS.yaml
 bat ./deployment/deployment-OOM.yaml
 
 
+#############################################################################################
+## RBAC
+
+https://niklaushirt.github.io/k8s_training_web/06-security/06-security-04
+
+
+rakkess resource pods --namespace default
+
+https://github.com/alcideio/rbac-tool/releases
+
+rbac-tool analysis -o table
+rbac-tool viz --include-namespaces default --include-pods-only --show-legend --show-psp --show-rule
+
+rbac-tool lookup -e '^kubeadm:.*'
+
+rbac-tool lookup -e '.*demo.*'
+
+
+bat ./deployment/critical.yaml
+kubectl apply -f ./deployment/critical.yaml
+
+
+
+#############################################################################################
+## SECURITY
+
+conftest test -p ./conftest/src/examples/kubernetes/policy ./deployment/demoapp.yaml
+
+
+kubescape list controls
+
+kubescape scan
+
+kubescape scan control C-0067,C-0066,C-0035,C-0012,C-0057,C-0048,C-0041 -v
+
+
+
+
+kubescape list frameworks
+
+kubescape scan framework DevOpsBest
+kubescape scan framework DevOpsBest -v
+kubescape scan framework nsa -v
+kubescape scan framework mitre -v
+kubescape scan framework cis-v1.23-t1.0.1 -v -f html> result.html
+open result.html
+
+
+
+# Minimize wildcard use in Roles
+kubescape scan control C-0187 -v
+
+# Privileged container
+kubescape scan control C-0057 -v
+
+# Ensure that the cluster-admin role is only used where required
+kubescape scan control C-0185 -v
+
+# Workload with administrative roles
+kubescape scan control C-0272 -v
+
+# Applications credentials in configuration file
+kubescape scan control C-0012 -v
 
 #############################################################################################
 ## DEBUG
 bat ./deployment/deployment-crashloop.yaml
 bat ./deployment/deployment-imagepull.yaml
 bat ./deployment/deployment-imagepull-sa.yaml
-
-
-
-
-
 
 
 
@@ -292,6 +341,9 @@ kubectl delete -f ./deployment/demoapp-service.yaml
 kubectl delete -f ./deployment/deployment.yaml
 kubectl delete -f ./deployment/deployment-cm.yaml
 kubectl delete -f ./deployment/pod.yaml
+kubectl delete -f ./deployment/service.yaml
+kubectl delete -f ./deployment/critical.yaml
+
 kubectl delete pods nginx-demo
 kubectl delete pods nginx
 podman --connection podman-machine-default-root kill portainer
